dfd97e3116f55d141f0cf3a2c232a1389c51925d
Mellty-511 <youshagangye1@gmail.com>
2026-05-01 15:31:29 +0900

    てｓｔだお

--- potato/Form1.Designer.vb
+++ potato/Form1.Designer.vb
@@ -29,14 +29,16 @@ Partial Class Form1
         Label2 = New Label()
         Button2 = New Button()
         ErrorProvider1 = New ErrorProvider(components)
+        Label3 = New Label()
         CType(ErrorProvider1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(286, 257)
+        Button1.Location = New Point(358, 321)
+        Button1.Margin = New Padding(4, 4, 4, 4)
         Button1.Name = "Button1"
-        Button1.Size = New Size(226, 102)
+        Button1.Size = New Size(282, 128)
         Button1.TabIndex = 0
         Button1.Text = "プレイする！"
         Button1.UseVisualStyleBackColor = True
@@ -44,33 +46,37 @@ Partial Class Form1
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(286, 103)
+        Label1.Location = New Point(358, 129)
+        Label1.Margin = New Padding(4, 0, 4, 0)
         Label1.Name = "Label1"
-        Label1.Size = New Size(204, 20)
+        Label1.Size = New Size(249, 25)
         Label1.TabIndex = 1
         Label1.Text = "かんたんブラックジャックfeat.ぽてち"
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(339, 182)
+        TextBox1.Location = New Point(424, 228)
+        TextBox1.Margin = New Padding(4, 4, 4, 4)
         TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(173, 27)
+        TextBox1.Size = New Size(215, 31)
         TextBox1.TabIndex = 2
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(278, 185)
+        Label2.Location = New Point(348, 231)
+        Label2.Margin = New Padding(4, 0, 4, 0)
         Label2.Name = "Label2"
-        Label2.Size = New Size(55, 20)
+        Label2.Size = New Size(60, 25)
         Label2.TabIndex = 3
-        Label2.Text = "さばのIP"
+        Label2.Text = "鯖のIP"
         ' 
         ' Button2
         ' 
-        Button2.Location = New Point(607, 255)
+        Button2.Location = New Point(759, 319)
+        Button2.Margin = New Padding(4, 4, 4, 4)
         Button2.Name = "Button2"
-        Button2.Size = New Size(130, 107)
+        Button2.Size = New Size(162, 134)
         Button2.TabIndex = 4
         Button2.Text = "管理者ぺーじ"
         Button2.UseVisualStyleBackColor = True
@@ -79,16 +85,28 @@ Partial Class Form1
         ' 
         ErrorProvider1.ContainerControl = Me
         ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(759, 108)
+        Label3.Margin = New Padding(4, 0, 4, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(89, 25)
+        Label3.TabIndex = 5
+        Label3.Text = "test( ´;ω;`)"
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(1000, 562)
+        Controls.Add(Label3)
         Controls.Add(Button2)
         Controls.Add(Label2)
         Controls.Add(TextBox1)
         Controls.Add(Label1)
         Controls.Add(Button1)
+        Margin = New Padding(4, 4, 4, 4)
         Name = "Form1"
         Text = "Form1"
         CType(ErrorProvider1, ComponentModel.ISupportInitialize).EndInit()
@@ -102,5 +120,6 @@ Partial Class Form1
     Friend WithEvents Label2 As Label
     Friend WithEvents Button2 As Button
     Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents Label3 As Label
 
 End Class
