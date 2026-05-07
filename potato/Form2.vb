1d631164d6d85894e5b26bf8674f99e8ee95b9f1
nanashi4762 <nanashi4762@gamil.com>
2026-05-07 15:54:46 +0900

    ui仮

--- potato/Form2.vb
+++ potato/Form2.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
 
 End Class
