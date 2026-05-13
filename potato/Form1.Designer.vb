b13561b98e64b10161343d319a7a6b0bd74021eb
Mellty-511 <youshagangye1@gmail.com>
2026-05-13 13:31:43 +0900

    結果にコミットする RIZAP

--- potato/Form1.Designer.vb
+++ potato/Form1.Designer.vb
@@ -29,14 +29,13 @@ Partial Class Form1
         Label2 = New Label()
         Button2 = New Button()
         ErrorProvider1 = New ErrorProvider(components)
-        Label3 = New Label()
         CType(ErrorProvider1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Button1
         ' 
         Button1.Location = New Point(358, 321)
-        Button1.Margin = New Padding(4, 4, 4, 4)
+        Button1.Margin = New Padding(4)
         Button1.Name = "Button1"
         Button1.Size = New Size(282, 128)
         Button1.TabIndex = 0
@@ -56,7 +55,7 @@ Partial Class Form1
         ' TextBox1
         ' 
         TextBox1.Location = New Point(424, 228)
-        TextBox1.Margin = New Padding(4, 4, 4, 4)
+        TextBox1.Margin = New Padding(4)
         TextBox1.Name = "TextBox1"
         TextBox1.Size = New Size(215, 31)
         TextBox1.TabIndex = 2
@@ -74,7 +73,7 @@ Partial Class Form1
         ' Button2
         ' 
         Button2.Location = New Point(759, 319)
-        Button2.Margin = New Padding(4, 4, 4, 4)
+        Button2.Margin = New Padding(4)
         Button2.Name = "Button2"
         Button2.Size = New Size(162, 134)
         Button2.TabIndex = 4
@@ -85,28 +84,17 @@ Partial Class Form1
         ' 
         ErrorProvider1.ContainerControl = Me
         ' 
-        ' Label3
-        ' 
-        Label3.AutoSize = True
-        Label3.Location = New Point(759, 108)
-        Label3.Margin = New Padding(4, 0, 4, 0)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(89, 25)
-        Label3.TabIndex = 5
-        Label3.Text = "test( ´;ω;`)"
-        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(1000, 562)
-        Controls.Add(Label3)
         Controls.Add(Button2)
         Controls.Add(Label2)
         Controls.Add(TextBox1)
         Controls.Add(Label1)
         Controls.Add(Button1)
-        Margin = New Padding(4, 4, 4, 4)
+        Margin = New Padding(4)
         Name = "Form1"
         Text = "Form1"
         CType(ErrorProvider1, ComponentModel.ISupportInitialize).EndInit()
@@ -120,6 +108,5 @@ Partial Class Form1
     Friend WithEvents Label2 As Label
     Friend WithEvents Button2 As Button
     Friend WithEvents ErrorProvider1 As ErrorProvider
-    Friend WithEvents Label3 As Label
 
 End Class
