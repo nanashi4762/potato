353a32a6193375ec41c89b1239d19119ef059abf
nanashi4762 <nanashi4762@gamil.com>
2026-05-15 15:54:57 +0900

    iroiro

--- potato/Form3.vb
+++ potato/Form3.vb
@@ -7,6 +7,7 @@ Public Class Form3
     Dim listener As TcpListener
     Dim clients As New List(Of TcpClient)
     Dim playerNames As New List(Of String)
+    Dim players As New Dictionary(Of TcpClient, Player)
     Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
             listener = New TcpListener(IPAddress.Any, Network.port)
@@ -41,12 +42,14 @@ Public Class Form3
                     Await client.GetStream().WriteAsync(res, 0, res.Length)
                 Else
                     playerNames.Add(name)
+                    players(client) = New Player With {.Name = name}
 
                     Dim res = Encoding.UTF8.GetBytes("NAME_OK:" & name)
                     Await client.GetStream().WriteAsync(res, 0, res.Length)
 
                     Dim joinMsg As String = "SYSTEM:" & name & " が参加しました" & vbCrLf
                     Dim joindata = Encoding.UTF8.GetBytes(joinMsg)
+                    WriteMessage("プレイヤー数: " & players.Count)
 
                     WriteMessage(joinMsg)
                     For Each c In clients.ToList()
@@ -109,4 +112,11 @@ Public Class Form3
         Next
     End Sub
 
+End Class
+
+Class Player
+    Public Name As String
+    Public Hand As New List(Of Integer)
+    Public IsStand As Boolean = False
+    Public IsPlaying As Boolean = False
 End Class
